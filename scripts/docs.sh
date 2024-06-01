5714bceb5999b2adbbfe2b9a07baa23ee13a4c01
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-06-01 16:29:48 +0200

    Create docs.sh

---
+++ scripts/docs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+cd "${0%/*}/.." || exit;
+jsdoc ./**/* -c ./.jsdoc/conf.json;
+for file in ./docs/**/*\&period\;*.html;
+do
+  mv "$file" "${file//\&period\;/.}";
+done;
+exit 0;
