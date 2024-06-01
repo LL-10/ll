1e2b19eaf95358cf3ad7d09de21fe879824af18d
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-06-01 10:28:35 +0200

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+	{
+		files: ["**/*.js"],
+		languageOptions: {
+			sourceType: "commonjs"
+		}
+	},
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		rules: {
+			"array-callback-return": "error",
+			"no-await-in-loop": "error",
+			"no-constructor-return": "error",
+			"no-duplicate-imports": "error",
+			"no-inner-declarations": "error",
+			"no-promise-executor-return": "error",
+			"no-self-compare": "error",
+			"no-template-curly-in-string": "error",
+			"no-unmodified-loop-condition": "error",
+			"no-unreachable-loop": "error",
+			"no-use-before-define": "error",
+			"no-useless-assignment": "error",
+			"require-atomic-updates": "error"
+		}
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended
+];
