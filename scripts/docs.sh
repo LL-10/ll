26e246831fba39020c3d8cf143da3012d17f7d97
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-06-03 18:12:36 +0200

    Update docs.sh
    indentation

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,6 +3,6 @@ cd "${0%/*}/.." || exit;
 jsdoc . -c ./.jsdoc/conf.json;
 for file in ./docs/**/*\&period\;*.html;
 do
-  mv "$file" "${file//\&period\;/.}";
+	mv "$file" "${file//\&period\;/.}";
 done;
 exit 0;
