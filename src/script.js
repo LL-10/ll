e5314bae9c75409dff4bd4977a850455fbd070be
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-06-16 11:41:08 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
-const webSocket = new WebSocket('ws://localhost:1337');
-webSocket.onopen = function send(event) {
-	if (event.isTrusted)
-		webSocket.send('IT WORKS!');
-};
-webSocket.onmessage = function receive(event) {
-	console.log(event.data);
-	webSocket.close();
-};
-//console.log(webSocket.bufferedAmount);
+function request(message) {
+	const webSocket = new WebSocket('ws://localhost:1337');
+	webSocket.onopen = function send(event) {
+		if (event.isTrusted)
+			webSocket.send(message);
+	};
+	webSocket.onmessage = function receive(event) {
+		console.log(event.data);
+		webSocket.close();
+	};
+}
