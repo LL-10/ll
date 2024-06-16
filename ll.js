24abacc765409b3de21cab82cbb64ea76e314169
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-06-16 11:31:21 +0200

    Update ll.js

--- ll.js
+++ ll.js
@@ -7,6 +7,32 @@ require('./core/base.js');
 const fs = require('fs');
 //const childProcess = require('child_process');
 
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+const server = http.createServer(function() {});
+server.listen(1337, function() {});
+new WebSocketServer({
+	httpServer: server,
+}).on('request', function(request) {
+	console.log(request.origin);
+	const connection = request.accept(null, request.origin);
+	connection.on('message', function(message) {
+		// Metodo eseguito alla ricezione di un messaggio
+		if (message.type === 'utf8') {
+			// Se il messaggio è una stringa, possiamo leggerlo come segue:
+			console.log(message.utf8Data);
+			connection.sendUTF('Perfect');
+		}
+		if (message.type === 'binary') {
+			console.log('Messaggio di dimensione: ' + message.binaryData.length + ' bytes');
+			connection.sendBytes(message.binaryData);
+		}
+	});
+	connection.on('close', function(connection) {
+		console.log('Connection ' + connection + ' closed');
+	});
+});
+
 const canvas = require('canvas');
 const Canvas = canvas.createCanvas;
 
