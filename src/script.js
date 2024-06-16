2f475f6d2e748d420c84e26c2d4c89f9bd323299
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-06-16 12:15:07 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,16 @@
+/**
+ * @file Set up client.
+ */
+
+/**
+ * Send request to server.
+ * @function request
+ * @argument {string|ArrayBuffer|object} message
+ * - The string or binary data to be sent to server.
+ * @example request('Hello world');
+ */
 function request(message) {
-	const webSocket = new WebSocket('ws://localhost:1337');
+	const webSocket = new globalThis.WebSocket('ws://localhost:1337');
 	webSocket.onopen = function send(event) {
 		if (event.isTrusted)
 			webSocket.send(message);
