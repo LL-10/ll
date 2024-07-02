972deaf462dd67f0e563c897b9fe070e15244fc3
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-07-02 11:51:49 +0200

    Create worker.js

---
+++ src/worker.js
@@ -0,0 +1,17 @@
+/*var i = 0;
+
+function timedCount() {
+	i = i + 1;
+	postMessage(i);
+	setTimeout("timedCount()",500);
+}
+
+timedCount();
+*/
+
+self.addEventListener('message', (event) => {
+	if (event.data.action === 'init')
+		self.canvas = new OffscreenCanvas(event.data.width, event.data.height);
+	else if (event.data.action === 'render')
+		postMessage(self.canvas, [self.canvas]);
+});
